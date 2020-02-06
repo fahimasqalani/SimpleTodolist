5340ff8978894a31062438a7e7612d300062e4df
unknown <fahim.asqalani@beyondsoft.com>
2020-02-06 17:05:30 +0800

    revert changes
    Signed-off-by: unknown <fahim.asqalani@beyondsoft.com>

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h3>Todo Tasks List</h3>
+            <h3>Tasks List</h3>
 
             <table class="table table-striped">
                 <tr>
