8bc0d7aed95481e218446e1562e59130a124291d
WAN <42267434+fahimasqalani@users.noreply.github.com>
2020-11-29 00:04:17 +0800

    Update
    wsd

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -7,17 +7,12 @@
         <div class="col-md-12">
             <h3>Add Task</h3>
             <p>Use the following form to add a new task to the system.</p>
-
             <hr>
-
             <form action="{{ url('postAdd') }}" method="post">
                 {{ csrf_field() }}
-
                 <p><input autofocus type="text" placeholder="Name..." name="name" class="form-control" /></p>
                 <p><input type="text" placeholder="Description..." name="description" class="form-control" /></p>
-
                 <hr>
-
                 <p><button class="form-control btn btn-success">Add Task</button></p>
             </form>
         </div>
